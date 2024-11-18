dbbdedeaf09ffe3305b133d2c32481aa124cb7c6
Nick <nickitrytry@gmail.com>
2024-11-18 16:16:01 +0300

    added MatrixCRS generation

---
+++ sparse-matrix/sparse-matrix/GenerateCRS.cpp
@@ -0,0 +1,45 @@
+#include "GenerateCRS.h"
+
+using namespace std;
+
+MatrixCRS generateCRS(int matrixSize, int countInRow)
+{
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> idis(0, matrixSize - 1);
+  uniform_real_distribution<> rdis(0, 1);
+  
+  int elCount = matrixSize * countInRow;
+  MatrixCRS m(matrixSize, elCount);
+ 
+  //заполнение значения столбцов
+  for (int i = 0; i < matrixSize; i++)
+  {
+	for (int j = 0; j < countInRow; j++)
+	{
+	  int f = 0;
+	  do
+	  {
+		m.Col()[i * countInRow + j] = idis(gen);
+		f = 0;
+		// проверка на повтор
+		for (int k = 0; k < j; k++)
+		{
+		  if (m.Col()[i * countInRow + j] == m.Col()[i * countInRow + k])
+			f == 1;
+		}
+	  } while (f == 1);
+	}
+
+	//сортировка массива столбцов в строке
+	sort(m.Col() + (countInRow * i), m.Col() + (countInRow * (i + 1)) );
+  }
+  //заполнение значений 
+  for (int i = 0; i < elCount; i++)
+	m.Values()[i] = idis(gen);
+  //заполнение индексов
+  for (int i = 0; i <= matrixSize; i++)
+	m.RowIndex()[i] = i * countInRow;
+
+  return m;
+}
---
+++ sparse-matrix/sparse-matrix/GenerateCRS.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "MatrixCRS.h"
+#include <random>
+#include <algorithm>
+
+MatrixCRS generateCRS(int matrixSize, int countInRow);
